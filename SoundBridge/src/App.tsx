7c55fe198ddb3c1e8681e656f47ff8546d8e0086
CalebScott0 <calebbenscott@gmail.com>
2026-02-06 20:41:36 -0600

    Dependency Setup

---
+++ SoundBridge/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'; // Import the button
+
+function App() {
+  return (
+    <div className='flex min-h-svh flex-col items-center justify-center'>
+      <Button>CLICK ME NOW OR ELSE</Button>
+    </div>
+  );
+}
+export default App;
