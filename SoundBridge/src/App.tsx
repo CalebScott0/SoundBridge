a85dd165fbccdc74b3c456a1b37aef5b61f1b7d7
13v311 <levellkensey06@gmail.com>
2026-02-07 11:01:11 -0600

    Choose roles and music genres

--- SoundBridge/src/App.tsx
+++ SoundBridge/src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import { StarterPage } from "@/components/StarterPage";
 import { SwipableCards } from "@/components/SwipableCards";
 import { Footer } from "@/components/Footer";
+import RoleButton  from "@/components/Role";
 
 function App() {
-  const [started, setStarted] = useState(false);
+  // const [started, setStarted] = useState(false);
 
   return (
     <>
-      {!started ? (
+      {/* {!started ? (
         <StarterPage onGetStarted={() => setStarted(true)} />
       ) : (
         <div className="min-h-svh justify-center bg-gradient-to-br from-slate-950 via-slate-900 to-indigo-950 px-4 py-10 text-white">
@@ -17,7 +18,47 @@ function App() {
           </div>
           <Footer />
         </div>
-      )}
+      )} */}
+        <div className="min-h-svh justify-center bg-gradient-to-br from-slate-950 via-slate-900 to-indigo-950 px-4 py-10 text-white">
+
+       <div style={{margin: "20px", fontSize: "24px"}}>
+        <p style={{fontWeight: "bold"}}>Choose your role</p>
+        <p>Select one or multiple roles that fit you as an artist</p>
+      </div>
+      <div className="flex flex-row items-center justify-center">
+        <RoleButton  name="Guitarist"/>
+        <RoleButton name="Drummer"/>
+        <RoleButton name="Vocalist"/>
+        <RoleButton name="Bassist"/>
+        <RoleButton name="Performer"/>
+      </div>
+
+      <div style={{margin:"20px", marginTop: "15%", fontSize: "24px"}}>
+        <p style={{fontWeight: "bold"}}>Choose your genres</p>
+        <p>Select one or multiple genres to describe the type of music you create</p>
+      </div>
+      <div className="flex flex-row items-center justify-center">
+        <RoleButton  name="Rock"/>
+        <RoleButton name="Pop"/>
+        <RoleButton name="Hip-Hop"/>
+        <RoleButton name="Jazz"/>
+        <RoleButton name="Electronic"/>
+      </div>
+        <div className="flex flex-row items-center justify-center">
+        <RoleButton  name="Indie"/>
+        <RoleButton name="Latin"/>
+        <RoleButton name="R&B/Soul"/>
+        <RoleButton name="Metal"/>
+        <RoleButton name="Acoustic"/>
+      </div>
+        <div className="flex flex-row items-center justify-center">
+          <RoleButton  name="Blues"/>
+          <RoleButton name="Trap"/>
+          <RoleButton name="Reggae"/>
+          <RoleButton name="K-pop"/>
+          <RoleButton name="Punk"/>
+      </div>
+      </div>
     </>
   );
 }
