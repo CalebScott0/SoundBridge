90339be8648c724e3e05fece685c202f4dfcafe2
13v311 <levellkensey06@gmail.com>
2026-02-07 09:47:02 -0600

    role button commit 1

--- SoundBridge/src/App.tsx
+++ SoundBridge/src/App.tsx
@@ -2,6 +2,10 @@ import { Button } from "@/components/ui/button"; // Import the button
 import { useEffect } from "react";
 import { auth } from "../firebase";
 import { ProfileCard } from "./components/ProfileCard";
+import { Progress } from "./components/ui/progress";
+import { Bold } from "lucide-react";
+import RoleButton from "./components/ui/rolebutton";
+import './components/ui/rolebutton.css';
 
 function App() {
   useEffect(() => {
@@ -9,10 +13,19 @@ function App() {
   });
   return (
     <>
-      <ProfileCard></ProfileCard>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>CLICK ME NOW OR ELSE</Button>
+      <div style={{margin: "20px", fontSize: "24px"}}>
+        <p style={{fontWeight: "bold"}}>Choose your role</p>
+        <p>Select one or multiple roles that fit you as an artist</p>
       </div>
+      <div className="flex min-h-svh flex-row items-center justify-center">
+        <RoleButton  name="Guitarist"/>
+        <RoleButton name="Drummer"/>
+        <RoleButton name="Vocalist"/>
+        <RoleButton name="Bassist"/>
+        <RoleButton name="Performer"/>
+      </div>
+
+
     </>
   );
 }
