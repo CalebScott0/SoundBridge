39025e5ef79e14ef63177fdb72e361f291e1516b
CalebScott0 <calebbenscott@gmail.com>
2026-02-06 22:40:26 -0600

    schema.txt JSON object mock up

--- SoundBridge/src/App.tsx
+++ SoundBridge/src/App.tsx
@@ -1,6 +1,11 @@
 import { Button } from '@/components/ui/button'; // Import the button
+import { useEffect } from 'react';
+import { auth } from './firebase';
 
 function App() {
+  useEffect(() => {
+    console.log('Firebase Auth Object', auth);
+  });
   return (
     <div className='flex min-h-svh flex-col items-center justify-center'>
       <Button>CLICK ME NOW OR ELSE</Button>
